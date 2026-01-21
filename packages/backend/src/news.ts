d0b27584f542b716cb1a32b82a7f208bcefb7569
Markus Sivula <markus.sivula@edu.savonia.fi>
2026-01-21 20:40:49 +0200

    Added User-Agent header in rss fetch

--- packages/backend/src/news.ts
+++ packages/backend/src/news.ts
@@ -15,6 +15,9 @@ export class NewsService {
     public static async fetchNewestNews(): Promise<NewsItem[]> {
         try {
             const response = await axios.get(this.NEWS_URL, {
+                headers: {
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+                },
                 responseType: "text"
             });
 
